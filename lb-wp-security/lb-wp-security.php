41653901c15675ee9122d88b63d14805f79e8f7f
Alec Dhuse <alecdhuse@gmail.com>
2018-08-17 11:28:29 -0400

    Added function to report IP
    Added function to report IP.
    Added read API key from file.

--- lb-wp-security/lb-wp-security.php
+++ lb-wp-security/lb-wp-security.php
@@ -4,14 +4,43 @@ Plugin Name: Little Bonsai WP Security
 
 */
 
-defined('ABSPATH') or die('Direct access is not allowed.');
+//defined('ABSPATH') or die('Direct access is not allowed.');
+
+function report_ip($api_key, $ip, $user_agent) {
+  $url = 'https://littlebonsai.co/api/v0.3/add_blacklist_ip.php';
+  $data = array('ip' => $ip, 'user_agent' => $user_agent, 'comment' => 'Failed WordPress Login', 'tags' => 'malicious-login', 'ref_url' => '');
+
+  // use key 'http' even if you send the request to https://...
+  $options = array(
+      'http' => array(
+          'header'  => "Content-type: application/json\r\nAuth: $api_key\r\n",
+          'method'  => 'POST',
+          'content' => http_build_query($data)
+      )
+  );
+
+  $context  = stream_context_create($options);
+  $result = file_get_contents($url, false, $context);
+  if ($result === FALSE) { /* Handle error */ }
+
+  var_dump($result);
+}
 
 function login_failed($username) {
   $ip = $_SERVER['REMOTE_ADDR'];
   $user_agent = $_SERVER['HTTP_USER_AGENT'];
 
+  $myfile = fopen("api.key", "r") or die("Error reading api key.");
+  $api_key = trim(fread($myfile,filesize("api.key")));
+  fclose($myfile);
+
+  report_ip($api_key, $ip, $user_agent);
 }
 
-add_action('wp_login_failed', 'login_failed');
+function init() {
+  add_action('wp_login_failed', 'login_failed');
+}
+
+init();
 
 ?>
