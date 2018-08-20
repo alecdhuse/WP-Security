ce0440d5b8eae417bd4500d1561697305a9bcc8e
Alec Dhuse <alecdhuse@gmail.com>
2018-08-20 14:15:59 -0400

    Added db access to store failed logins and report after 3 attempts
    Added db access to store failed logins and report after 3 attempts

--- lb-wp-security/includes/class-lb-wp-security-activator.php
+++ lb-wp-security/includes/class-lb-wp-security-activator.php
@@ -31,6 +31,25 @@ class LB_WP_Security_Activator {
 	 */
 	public static function activate() {
 
+		/* Create database tables */
+		global $wpdb;
+
+    $table_name = $wpdb->prefix . "littlebonsai_failed_logins";
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE $table_name (
+		  id mediumint(9) NOT NULL AUTO_INCREMENT,
+			ip tinytext NOT NULL,
+			user_agent text NOT NULL,
+			seen_count mediumint(9) DEFAULT '1' NOT NULL,
+		  first_seen timestamp DEFAULT 0,
+			last_seen timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+			reported boolean DEFAULT False NOT NULL,
+		  PRIMARY KEY  (id)
+		) $charset_collate;";
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
 	}
 
 }
